3ab37082178f9fc43693a29b249064f9d74f59df
Daniel D <daniel.diepold@gmail.com>
2011-07-05 17:25:18 +0200

    order parameters by name for checksum calculation

--- src/edacc/parameterspace/ParameterConfiguration.java
+++ src/edacc/parameterspace/ParameterConfiguration.java
@@ -25,7 +25,8 @@ public class ParameterConfiguration {
 	public void updateChecksum() {
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			for (Parameter p: parameter_instances.keySet()) {
+			List<Parameter> sortedKeys = new ArrayList(parameter_instances.keySet());
+			for (Parameter p: sortedKeys) {
 				if (parameter_instances.get(p) != null) {
 					md.update(parameter_instances.get(p).toString().getBytes());
 				}
