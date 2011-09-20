c5746c11b19109f547f14d62251e36fe77f49322
Adrian Balint <balint@.(none)>
2011-09-20 15:22:47 +0200

    changed costs function to compute only the cost of finished job independent on the jobs type that are passed

--- src/edacc/api/costfunctions/Average.java
+++ src/edacc/api/costfunctions/Average.java
@@ -9,14 +9,19 @@ public class Average implements CostFunction {
 	public float calculateCost(List<ExperimentResult> results) {
 		float sum = 0.0f;
 		if (results.size() == 0) return 0;
-		for (ExperimentResult res: results) sum += res.getResultTime();
+		for (ExperimentResult res: results) 
+			if (res.getStatus().getStatusCode() > 0){ 
+				sum += res.getResultTime();
+			}
 		return sum / results.size();
 	}
 	@Override
 	public float calculateCumulatedCost(List<ExperimentResult> results) {
 		float sum = 0.0f;
 		if (results.size() == 0) return 0;
-		for (ExperimentResult res: results) sum += res.getResultTime();
+		for (ExperimentResult res: results) 
+			if (res.getStatus().getStatusCode() > 0)
+				sum += res.getResultTime();
 		return sum ;
 	}
 	
--- src/edacc/api/costfunctions/Median.java
+++ src/edacc/api/costfunctions/Median.java
@@ -10,21 +10,28 @@ public class Median implements CostFunction {
 
 	@Override
 	public float calculateCost(List<ExperimentResult> results) {
-		if (results.size() == 0) return 0;
+		if (results.size() == 0)
+			return 0;
 		List<Float> vals = new ArrayList<Float>();
-		for (ExperimentResult res: results) {
-			vals.add(res.getResultTime());
+		for (ExperimentResult res : results) {
+			if (res.getStatus().getStatusCode() > 0) {
+				vals.add(res.getResultTime());
+			}
 		}
 		Collections.sort(vals);
 		return vals.get(vals.size() / 2);
 	}
-	
+
 	@Override
 	public float calculateCumulatedCost(List<ExperimentResult> results) {
 		float sum = 0.0f;
-		if (results.size() == 0) return 0;
-		for (ExperimentResult res: results) sum += res.getResultTime();
-		return sum ;
+		if (results.size() == 0)
+			return 0;
+		for (ExperimentResult res : results)
+			if (res.getStatus().getStatusCode() > 0) {
+				sum += res.getResultTime();
+			}
+		return sum;
 	}
 
 	@Override
